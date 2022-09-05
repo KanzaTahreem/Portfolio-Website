f60faeb928ab6f7ad50b0f00d993a252a79ff5d2
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-09-06 02:19:15 +0500

    Add load event

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-const barMenu = document.querySelector('.bar-menu');
-const popUp = document.querySelector('.pop-up');
-const close = document.querySelector('.close');
-const menu = document.querySelector('.menu');
+window.onload = () => {
+  const barMenu = document.querySelector('.bar-menu');
+  const popUp = document.querySelector('.pop-up');
+  const close = document.querySelector('.close');
+  const menu = document.querySelector('.menu');
 
-barMenu.addEventListener('click', () => {
-  popUp.classList.toggle('hidden');
-});
+  barMenu.addEventListener('click', () => {
+    popUp.classList.remove('hidden');
+  });
 
-close.addEventListener('click', () => {
-  popUp.classList.toggle('hidden');
-});
+  close.addEventListener('click', () => {
+    popUp.classList.remove('hidden');
+  });
 
-menu.addEventListener('click', () => {
-  popUp.classList.toggle('hidden');
-});
+  menu.addEventListener('click', () => {
+    popUp.classList.add('hidden');
+  });
+};
